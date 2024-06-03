1da3945f3b74ce714b0eed36cf6ead46497b7bd1
Dominik Wojcieszak <xxhkr1xx@gmail.com>
2024-06-03 19:59:42 +0200

    usuniecie komentarza

--- MusicDB/Album.cs
+++ MusicDB/Album.cs
@@ -6,7 +6,7 @@ namespace MusicCollectionManager
     public struct Album
     {
         public string Title { get; set; }
-        public string Type { get; set; } // CD or DVD
+        public string Type { get; set; } 
         public TimeSpan? Duration { get; set; }
         public List<Track> Tracks { get; set; }
         public List<string> Performers { get; set; }
